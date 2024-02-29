93e87a96c70a31f42a78dfa83d16866f4400d628
ChiaraBurni <chiaraburni@gmail.com>
2024-02-28 22:31:50 -0300

    primera pre entrega

--- src/api/products/router.js
+++ src/api/products/router.js
@@ -28,11 +28,11 @@ router.post("/", async (req, res) => {
   const newProduct = req.body
   try {
     await newProductManager.addProduct(newProduct)
-    res.send({status: "success", message: "Correctly aggregated product"})
+    res.send({status: "success", message: "Producto agregado correctamente"})
   } catch (error) {
-    if (error.message === "Product already exists") {
+    if (error.message === "Producto ya existe") {
       res.status(409).json({ error: `${error.message}` })
-    } else if (error.message === "Product missing fields") {
+    } else if (error.message === "Producto incompleto") {
       res.status(409).json({ error: `${error.message}` })
     } else {
       res.status(500).send({ status: "error", message: "Internal Server Error" })
@@ -45,7 +45,7 @@ router.put("/:pid", async (req, res) => {
   const updatedProduct = req.body
   try {
     await newProductManager.updateProduct(pid, updatedProduct)
-    res.send({status: "success", message: "Correctly updated product"})
+    res.send({status: "success", message: "Producto agregado correctamente"})
   } catch (error) {
     res.status(409).json({ error: `${error.message}` })
   }
@@ -55,7 +55,7 @@ router.delete("/:pid", async (req, res) => {
   let pid = req.params.pid
   try {
     await newProductManager.deleteProduct(pid)
-    res.send({status: "success", message: "Correctly deleted product"})
+    res.send({status: "success", message: "Producto eliminado correctamente"})
   } catch (error) {
     res.status(409).json({ error: `${error.message}` })
   }
